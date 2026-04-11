817b68ab6d6487d3988b0b1826ed01b0207ecbd7
VictorRMarcelino <httpsvictorramos@gmail.com>
2026-04-11 16:00:10 -0300

    Removed wrong migration folder

--- src/main/java/com/mywallet/api/migration/create_table_wallet_04_2026.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS WALLET;
-
-CREATE TABLE WALLET (
-    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    balance numeric(8,2) NOT NULL,
-	user_id UUID not null
-);
-
-ALTER TABLE WALLET ADD CONSTRAINT FK_WALLET_USER FOREIGN KEY (user_id) references users(id)
